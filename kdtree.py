d60cfbdbcf94bd56b818a77b20a6ffa213c639e2
Dukosli <aethunder1648@gmail.com>
2025-09-20 14:52:42 -0400

    mergesort by depth working

--- kdtree.py
+++ kdtree.py
@@ -4,7 +4,8 @@
 
 
 
-# From G4G, change to sort by k-d depth
+# From G4G, change to sort by k-d depth at 
+# level to find median by dimension at every level
 def merge(arr, left, mid, right):
     n1 = mid - left + 1
     n2 = right - mid
@@ -34,20 +35,18 @@ def merge(arr, left, mid, right):
             j += 1
         k += 1
 
-    # Copy the remaining elements of L[],
-    # if there are any
+    # Dump remainder after comparison burns
     while i < n1:
         arr[k] = L[i]
         i += 1
         k += 1
 
-    # Copy the remaining elements of R[], 
-    # if there are any
     while j < n2:
         arr[k] = R[j]
         j += 1
         k += 1
 
+# Head
 def mergeSort(arr, left, right):
     if left < right:
         mid = (left + right) // 2
@@ -56,24 +55,11 @@ def mergeSort(arr, left, right):
         mergeSort(arr, mid + 1, right)
         merge(arr, left, mid, right)
         
+# Sort by depth, temp changes as passed by
 def sortByDepth(dict_in, temp, depth):
-    temp = [0] * len(dict_in)
-    i = 0
+    temp.clear()
     
     for key, value in dict_in.items():
-        temp[i] = [key, value[depth]]
-        i += 1
+        temp.append([key, value[depth]])
         
     mergeSort(temp, 0, len(temp) - 1)
-    for i in temp:
-        print(i)
-    
-newDict = {
-    1:[1,2,3],
-    3:[5,4,2],
-    2:[10,1,1] }
-
-tempor = []
-sortByDepth(newDict, tempor, 2)
-for i in tempor:
-    print(i[0] + " ")
