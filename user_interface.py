8cdc8934885d2a5d8f2ec6cc37c1e8e9871ce789
srejames <ethan.hidalgo@gmail.com>
2025-09-21 01:23:30 -0400

    title change

--- user_interface.py
+++ user_interface.py
@@ -108,7 +108,7 @@ def restart():
 
 
 
-st.title("Movie Recommender")
+st.markdown("<h1 style='text-align: center; color: #0A84FF;'>🎬 Movie Recommender</h1>", unsafe_allow_html=True)
 
 st.write(
     "### Tell us what kinds of movies you’re in the mood for:\n"
