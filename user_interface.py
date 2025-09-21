3a60c4eb72e192b994642d68c7a463365d1ffb45
srejames <ethan.hidalgo@gmail.com>
2025-09-21 11:16:43 -0400

    style

--- user_interface.py
+++ user_interface.py
@@ -192,8 +192,8 @@ if st.session_state["results"]:
 if st.session_state["locked"]:
     st.button("Restart", on_click=restart)
 
-col_1, col_2, _ = st.columns([1, 1, 8])
+col_1, col_2, _ = st.columns([1, 1, 10])
 with col_1:
-    st.markdown("Credits:")
+    st.markdown("###### Credits:")
 with col_2:
     st.image("movide_db_img.svg", width=120)
